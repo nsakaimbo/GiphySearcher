7570de279db213cd9084e214e5c9359e6d43350e
nsakaimbo <nick@lemon.co>
2016-07-15 15:30:34 -0400

    Initial Commit

---
+++ GiphySearcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiphySearcher
+//
+//  Created by Nicholas Sakaimbo on 7/15/16.
+//  Copyright © 2016 nick_skmbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
